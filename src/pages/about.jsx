657515368a04d50dd855c89d5f38936b08d57842
imamusthofaa <imamusthofaa660@gmail.com>
2025-07-18 10:43:37 +0700

    About imam

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,7 +1,25 @@
 import "../App.css";
 
 function About() {
-  return <h1>ubah dari punya tiara About</h1>;
+  return (
+    <div className="" style={{ marginTop: "50px" }}>
+      <h1
+        className="about-custom"
+        style={{ textAlign: "left", fontSize: "50px" }}
+      >
+        About
+      </h1>
+      <div className="text-about grid place-content-center border-t border-base-300 h-80">
+        Di TravelSkutt, kami percaya bahwa setiap perjalanan bukan cuma soal
+        tempat baru, tapi juga cerita yang akan lu bawa pulang. Kami hadir buat
+        bantu lu wujudin liburan impian, mulai dari tiket pesawat, penginapan,
+        sampai itinerary yang seru dan nggak ribet. Dengan tim berpengalaman dan
+        mitra terpercaya di berbagai negara, kami siap nemenin langkah lu ke
+        mana pun tujuan lu. Mau solo traveling, honeymoon, atau liburan
+        keluarga? Kami siap bantu semua. Jelajahi dunia, mulai dari sini.
+      </div>
+    </div>
+  );
 }
 
 export default About;
