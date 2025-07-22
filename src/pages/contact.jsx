261679b9d66187261b25510643ae4dc1cf2e6538
iball2309 <putrafinanto@gmail.com>
2025-07-22 11:25:52 +0700

    uvuvweve

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import { FaEnvelope, FaMapMarkerAlt } from 'react-icons/fa';
-import '../contact.css'; // sesuaikan path jika beda folder
+import React from "react";
+import { FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+import "../contact.css"; // sesuaikan path jika beda folder
 
 const Contact = () => {
   return (
-    <div className="contact-wrapper">
+    <div className="contact-wrapper" style={{ marginTop: "30px" }}>
       <div className="contact-container">
         <h1 className="contact-title">
           Contact us <FaEnvelope className="title-icon" />
         </h1>
 
         <p className="contact-description">
-          Setiap langkah adalah awal dari kisah baru. Di <strong>TravelSkutt</strong>, kami percaya bahwa perjalanan bukan sekadar berpindah tempat, tapi tentang makna yang tumbuh di setiap momen.
-          <br /><br />
-          Kalau kamu punya pertanyaan, butuh bantuan buat nyusun rencana liburan, atau sekadar ingin ngobrol dulu, kami selalu ada. Jangan ragu menghubungi kami, karena langkah kecil hari ini bisa jadi awal petualangan luar biasa.
+          Setiap langkah adalah awal dari kisah baru. Di{" "}
+          <strong>TravelSkutt</strong>, kami percaya bahwa perjalanan bukan
+          sekadar berpindah tempat, tapi tentang makna yang tumbuh di setiap
+          momen.
+          <br />
+          <br />
+          Kalau kamu punya pertanyaan, butuh bantuan buat nyusun rencana
+          liburan, atau sekadar ingin ngobrol dulu, kami selalu ada. Jangan ragu
+          menghubungi kami, karena langkah kecil hari ini bisa jadi awal
+          petualangan luar biasa.
         </p>
 
         <div className="contact-info">
@@ -23,7 +30,9 @@ const Contact = () => {
           </div>
           <div className="contact-info-item">
             <FaMapMarkerAlt className="info-icon" />
-            <span>Jam operasional: <strong>Senin–Jumat, 9 pagi – 5 sore</strong></span>
+            <span>
+              Jam operasional: <strong>Senin–Jumat, 9 pagi – 5 sore</strong>
+            </span>
           </div>
         </div>
 
